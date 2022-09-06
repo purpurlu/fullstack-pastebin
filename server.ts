ac95560554f86249ca3b602804c2b419ba603d7d
purpurlu <laurazurbayeva@gmail.com>
2022-09-06 11:00:20 +0200

    add empty body limit

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ config(); //Read .env file lines as though they were env vars.
 const herokuSSLSetting = { rejectUnauthorized: false }
 const sslSetting = process.env.LOCAL ? false : herokuSSLSetting
 const dbConfig = {
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.LOCAL ? process.env.DATABASE_URL_LOCAL : process.env.DATABASE_URL,
   ssl: sslSetting,
   //ssl: false,
 };
@@ -27,10 +27,14 @@ app.use(cors()) //add CORS support to each following route handler
 const client = new Client(dbConfig);
 client.connect();
 
+// app.get("/", async (req, res)=> {
+  
+// })
+
 app.get("/pastes", async (req, res) => {
   try {
-    const dbres = await client.query('select title, body from pastes LIMIT 10');
-    res.json(dbres.rows);
+    const dbres = await client.query('select title, body from pastes ORDER BY id_paste DESC LIMIT 10');
+    res.status(200).json(dbres.rows);
   }
   catch (err) {
     console.error(err)
@@ -40,10 +44,14 @@ app.get("/pastes", async (req, res) => {
 
 app.post("/pastes", async(req, res) => {
   try {
-
       const {title, body} = req.body
-      const newPaste = await client.query("INSERT INTO pastes (title, body) VALUES($1, $2) RETURNING *", [title, body])
-      res.json(newPaste.rows)
+      if (body !== ("")) {
+        const newPaste = await client.query("INSERT INTO pastes (title, body) VALUES($1, $2) RETURNING *", [title, body])
+        res.status(200).json(newPaste.rows)
+      }
+      else {
+        res.status(500).json("THE BODY CANNOT BE EMPTY")
+      }
     }
   catch (err) {
     console.error(err)
